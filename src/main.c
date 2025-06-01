fb06ca7ef09e8c076895ae9a46dfeae38bc6ef97
Hemanthraj2003 <hemanth3raj@gmail.com>
2025-06-02 05:12:17 +0530

    second initial commit

---
+++ src/main.c
@@ -0,0 +1,8 @@
+// entry of the Project
+
+#include <stdio.h>
+
+int main(){
+	printfW("Welcome to MyMalloc\nHere we are building a custom Malloc in order to understand memory managenemt in C");
+	return 0;
+}
