adb81877e907a182729ed95cd6eaf469b87aba29
Hemanthraj2003 <hemanth3raj@gmail.com>
2025-06-04 23:05:34 +0530

    mymalloc function

--- src/main.c
+++ src/main.c
@@ -1,8 +1,32 @@
 // entry of the Project
 
 #include <stdio.h>
+#include <mymalloc.h>
 
-int main(){
-	printfW("Welcome to MyMalloc\nHere we are building a custom Malloc in order to understand memory managenemt in C");
-	return 0;
+#define MEMORY_SIZE 1024 * 1024 // 1MB of virual memory for heap
+
+// using static to keep memory and offset for the entirity of the program...  
+static char memory[MEMORY_SIZE]; // heap
+
+// points to the start point of unused memory...
+static size_t memory_offset = 1; 
+
+// this function will allocate memorry based on the parameter
+void* my_malloc(size_t size){
+	//not enough memory or invalid memory size checker
+	if(size == 0 || size + memory_offset > MEMORY_SIZE){
+		return NULL; 
+	}
+
+	int *ptr = &memory[memory_offset];
+	memory_offset = size + memory_offset;
+	retrun ptr;
+}
+
+void my_free(void* ptr){
+	
+}
+
+void* my_realloc(void *ptr, size_t size){
+	return NULL;
 }
